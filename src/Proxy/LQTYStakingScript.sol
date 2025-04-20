d0c2f77c0f2ca8753feb1adf96941d1a033a4b6e
sushmitsarmah <sushmitsarmah@gmail.com>
2025-04-20 15:49:54 +0530

    added proxy folder

---
+++ src/Proxy/LQTYStakingScript.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+import "../Dependencies/CheckContract.sol";
+import "../Interfaces/ILQTYStaking.sol";
+
+
+contract LQTYStakingScript is CheckContract {
+    ILQTYStaking immutable LQTYStaking;
+
+    constructor(address _lqtyStakingAddress) {
+        checkContract(_lqtyStakingAddress);
+        LQTYStaking = ILQTYStaking(_lqtyStakingAddress);
+    }
+
+    function stake(uint _LQTYamount) external {
+        LQTYStaking.stake(_LQTYamount);
+    }
+}
---
+++ src/Proxy/TokenScript.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
+import "../Dependencies/CheckContract.sol";
+import "../Dependencies/IERC20.sol";
+
+
+contract TokenScript is CheckContract {
+    string constant public NAME = "TokenScript";
+
+    IERC20 immutable token;
+
+    constructor(address _tokenAddress) {
+        checkContract(_tokenAddress);
+        token = IERC20(_tokenAddress);
+    }
+
+    function transfer(address recipient, uint256 amount) external returns (bool) {
+        return token.transfer(recipient, amount);
+    }
+
+    function allowance(address owner, address spender) external view returns (uint256) {
+        return token.allowance(owner, spender);
+    }
+
+    function approve(address spender, uint256 amount) external returns (bool) {
+        return token.approve(spender, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
+        return token.transferFrom(sender, recipient, amount);
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) external returns (bool) {
+        return token.increaseAllowance(spender, addedValue);
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool) {
+        return token.decreaseAllowance(spender, subtractedValue);
+    }
+}
